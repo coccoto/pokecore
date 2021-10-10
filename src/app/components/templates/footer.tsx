a8c89e3c1804924c0f590b37dae106c8ce1c5539
coccoto <soprano.lune@gmail.com>
2021-10-10 20:00:10 +0900

    2021-10-10 20:00

---
+++ src/app/components/templates/footer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+export default () => {
+
+    return (
+        <footer>footer</footer>
+    )
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// molecules
+import Form from '@src/app/components/molecules/form'
+// templates
+import Header from '@src/app/components/templates/header'
+import Footer from '@src/app/components/templates/footer'
 
 export default () => {
 
     return (
-        <h1>TEST</h1>
+        <Router>
+            <Header></Header>
+
+            <Switch>
+                <Route exact path={'/'}>
+                    <Form></Form>
+                </Route>
+            </Switch>
+
+            <Footer></Footer>
+        </Router>
     )
 }
